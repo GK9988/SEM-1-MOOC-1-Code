003f1fe0293cf070bf507f939e91976d31a19d6c
Garv Khurana <gk12345garvkhurana@gmail.com>
2021-09-04 21:10:56 +0530

    ex-3 express

---
+++ Module - 1/Excercise - 3/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import http from "http";
+import morgan from "morgan";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const hostname = "localhost";
+const port = 3000;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Servers</h1></body></html>");
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
